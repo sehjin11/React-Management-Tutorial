5aebd513077c76f14321ceaa9638c14c289eb5c4
Jin <sehjin11@gmail.com>
2021-12-11 17:47:44 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://picsum.photos/200",
+    name: "김철수",
+    birthday: "870422",
+    gender: "남자",
+    job: "회사원",
+  },
+  {
+    id: 2,
+    image: "https://picsum.photos/200",
+    name: "이영희",
+    birthday: "920122",
+    gender: "여자",
+    job: "인턴",
+  },
+  {
+    id: 3,
+    image: "https://picsum.photos/200",
+    name: "최영수",
+    birthday: "970630",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
