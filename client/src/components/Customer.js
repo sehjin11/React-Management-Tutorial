901cb39727456793f58b2c5ec49202e0fa5faac5
Jin <sehjin11@gmail.com>
2021-12-11 23:46:35 +0900

    Add Node.js Express Server

---
+++ client/src/components/Customer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+
+class Customer extends React.Component {
+  render() {
+    return (
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img src={this.props.image} alt="profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
+    );
+  }
+}
+
+// class CustomerProfile extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <img src={this.props.image} alt="profile" />
+//         {this.props.name}({this.props.id}){this.props.name}
+//       </div>
+//     );
+//   }
+// }
+
+// class CustomerInfo extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         {this.props.birthday}
+//         {this.props.gender}
+//         {this.props.job}
+//       </div>
+//     );
+//   }
+// }
+
+export default Customer;
