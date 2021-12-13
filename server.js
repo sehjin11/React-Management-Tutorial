0f05b714bba57adc664bc2e1df479a41753d865e
Jin <sehjin11@gmail.com>
2021-12-13 21:21:46 +0900

    Mysql DB Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -5,33 +6,24 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect;
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://picsum.photos/200",
-      name: "김철수",
-      birthday: "870422",
-      gender: "남자",
-      job: "회사원",
-    },
-    {
-      id: 2,
-      image: "https://picsum.photos/200",
-      name: "이영희",
-      birthday: "920122",
-      gender: "여자",
-      job: "인턴",
-    },
-    {
-      id: 3,
-      image: "https://picsum.photos/200",
-      name: "최영수",
-      birthday: "970630",
-      gender: "남자",
-      job: "대학생",
-    },
-  ]);
+  connection.query("select * from customer", (err, rows, fields) => {
+    res.send(rows);
+    console.log(err);
+  });
 });
 
 app.listen(5000, () => console.log("Listening on port 5000"));
